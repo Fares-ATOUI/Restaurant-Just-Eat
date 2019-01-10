766d891bdf9353209e7565e01d3b77cf9a0465ba
Fares-ATOUI <46579517+Fares-ATOUI@users.noreply.github.com>
2019-01-10 23:20:57 +0100

    Add files via upload

---
+++ Just-Eat-V1.0/src/components/Alert.vue
@@ -0,0 +1,25 @@
+<!----------------------------------------html------------------------------------------------------------------------------------------------------->
+<template>
+  <div class="alert alert-warning alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    {{message}}
+  </div>
+</template>
+
+<!----------------------------------------Script-------------------------------------------------------------------------------------------------------->
+<script>
+  export default {
+    name: 'Alert',
+    props: ['message'],
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!----------------------------------------Style--------------------------------------------------------------------------------------------------------->
+<style scoped>
+
+</style>
